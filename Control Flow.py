657b5522fe0834e706ef785d6cab8b74daac5cb4
Kyle <02kyle.vanlandegent@gulllakecs.org>
2021-10-21 12:51:00 -0400

    Add Print statement for ATM

--- Control Flow.py
+++ Control Flow.py
@@ -13,9 +13,11 @@ print("Welcone to Cash-R Us Bank\n\nLet's take a moment to set up your account.\
 firstName = input("What is your first name: ")
 lastName = input("What is your last name: ")
 
-print("\nWelcome to Cash-R-Us", firstName,lastName + "we will now set up a security PIN on your account.\n")
+print("\nWelcome to Cash-R-Us", firstName,lastName, + "we will now set up a security PIN on your account.\n")
 
 # Set up a PIN - Personal Identification Number
 pin = input("Please choose a 4 digit PIN:")
 
 print("\nThank you",firstName + ", we see that you set your PIN to",pin)
+
+print("\n would you like to make a transaction through our automated teller machine")
