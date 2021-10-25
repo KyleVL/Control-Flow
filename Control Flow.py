ef11268b5bafe1d0e3df77150fcf084cfaa6dffd
Kyle <02kyle.vanlandegent@gulllakecs.org>
2021-10-25 12:48:54 -0400

    Add atm variable and if, else statements

--- Control Flow.py
+++ Control Flow.py
@@ -13,11 +13,19 @@ print("Welcone to Cash-R Us Bank\n\nLet's take a moment to set up your account.\
 firstName = input("What is your first name: ")
 lastName = input("What is your last name: ")
 
-print("\nWelcome to Cash-R-Us", firstName,lastName, + "we will now set up a security PIN on your account.\n")
+print("\nWelcome to Cash-R-Us", firstName,lastName + " we will now set up a security PIN on your account.\n")
 
 # Set up a PIN - Personal Identification Number
 pin = input("Please choose a 4 digit PIN:")
 
 print("\nThank you",firstName + ", we see that you set your PIN to",pin)
 
-print("\n would you like to make a transaction through our automated teller machine")
+print("\nwould you like to make a transaction through our automated teller machine")
+atm = input("Yes or No: ").upper()
+
+if atm == "YES":
+    print("\n-----------------------------------------------------------------------------------------------------\n")
+
+
+else:
+    print("\nHave a wonderful day" ,firstName, lastName, "please come back soon")
